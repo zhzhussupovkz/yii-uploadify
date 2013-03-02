a58ab34ffccfaa2cae3cbdbf601d35b1b412e889
Zhussupov Zhassulan <zhzhussupovkz@gmail.com>
2013-03-02 16:46:57 +0400

    v1.0

---
+++ yiiUploadify.php
@@ -0,0 +1,61 @@
+<?php
+
+class yiiUploadify extends CInputWidget
+{
+	//input file name
+	public $name = 'file_upload';
+
+	//button text
+	public $buttonText = 'Hello world!';
+
+	//AJAX URL
+	public $ajaxUrl;
+
+	//button height
+	public $height = '30';
+
+	//button width
+	public $width = '120';
+
+	//path to uploadify.swf
+	protected  $swfPath;
+
+	//id
+	protected $id = 'file_upload';
+
+	//run widget
+	public function run()
+	{
+		echo '<div id="queue" class = "uploadify"></div>';
+		echo CHtml::fileField($this->name, '', array('id' => $this->id));
+		$this->allScripts();
+		$script = '$(function(){
+		$("#'.$this->id.'").uploadify({
+			height	: '.$this->height.',
+			width : '.$this->width.',
+			buttonText : "'.$this->buttonText.'",
+			swf : "'.$this->swfPath.'",
+			uploader : "'.$this->ajaxUrl.'"
+		});
+		});';
+		Yii::app()->clientScript->registerScript('yiiUploadify', $script, CClientScript::POS_HEAD);
+	}
+
+	//access scripts
+	protected function allScripts()
+	{
+		$assets=dirname(__FILE__).DIRECTORY_SEPARATOR.'assets';
+		$baseUrl=Yii::app()->assetManager->publish($assets);
+		if(is_dir($assets))
+		{
+			Yii::app()->clientScript->registerCoreScript('jquery');
+			Yii::app()->clientScript->registerScriptFile($baseUrl.'/js/jquery.uploadify.min.js', CClientScript::POS_HEAD);
+			Yii::app()->clientScript->registerCssFile($baseUrl.'/css/uploadify.css');
+			$this->swfPath = $baseUrl.'/uploadify.swf';
+		}
+		else
+		{
+			throw new Exception('Error in yiiUploadify widget! Cannot access assets folder');
+		}
+	}
+}
